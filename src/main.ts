d930ad2fc2adf9b0e156aed42b85c30994d74f0b
Edward Zhou <esz7923@nyu.edu>
2026-03-17 15:20:58 -0400

    Initial three.js project setup

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const FOV = 75;
+const NEAR = 0.1;
+const FAR = 1000;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, NEAR, FAR);
+camera.position.set(5, 5, 5);
+
+const canvas = document.getElementById("app") || undefined;
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight, false);
+renderer.setAnimationLoop(animate);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+scene.add(new THREE.GridHelper(100, 20));
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(1, 1, 1);
+scene.add(light);
+scene.add(new THREE.AmbientLight(0x888888));
+
+const geometry = new THREE.SphereGeometry();
+const material = new THREE.MeshStandardMaterial({ color: 0x8888ff });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+function animate() {
+  // controls.update();
+
+  if (resizeRenderer(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera);
+}
+
+function resizeRenderer(renderer: THREE.WebGLRenderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+animate();
